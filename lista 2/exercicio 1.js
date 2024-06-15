9aec6c85a020dddab2ac5ca3b7109a5cff0e8831
biaanna <biaanna104@gmail.com>
2024-06-14 23:30:22 -0300

    exercício 1

---
+++ lista 2/exercicio 1.js
@@ -0,0 +1,8 @@
+console.log("Farmacia");
+
+let produto1= "Dipirona (comprimido)";
+let preco= 13.90;
+
+let precopromocional = Math.trunc(preco);
+console.log(precopromocional)
+console.log("2x Dipirona " + precopromocional*2)
