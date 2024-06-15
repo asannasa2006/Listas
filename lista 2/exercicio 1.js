869925b99a7f7041a5423eef5f40e793631d8d78
biaanna <biaanna104@gmail.com>
2024-06-14 23:59:02 -0300

    Exercício 2

--- lista 2/exercicio 1.js
+++ lista 2/exercicio 1.js
@@ -5,4 +5,5 @@ let preco= 13.90;
 
 let precopromocional = Math.trunc(preco);
 console.log(precopromocional)
+console.log("Diporona = " + preco)
 console.log("2x Dipirona " + precopromocional*2)
---
+++ lista 2/exercicio2.js
@@ -0,0 +1,10 @@
+console.log( "Lan House")
+
+const valorintegral = 2
+
+let tempodeUso= parseInt(prompt("Tempo de uso:"))
+let tempoIntegral= tempodeUso/15
+let tempoPagar= Math.ceil(tempodeUso)
+
+console.log("tempo de uso =" + tempodeUso)
+console.log("valor a pagar = " + (tempoIntegral*2))
