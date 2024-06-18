cfeb672e30632710b32e7639d425e0115819df7d
asannasa2006 <163129477+asannasa2006@users.noreply.github.com>
2024-06-18 11:05:40 -0300

    Update exercicio 1.js

--- lista 2/exercicio 1.js
+++ lista 2/exercicio 1.js
@@ -6,5 +6,5 @@ let preco= 13.90;
 let precopromocional = Math.trunc(preco);
 
 console.log("Valor:" + preco)
-console.log("Preço promocional " +precopromocional)
+console.log("Preço promocional: " +precopromocional)
 console.log("2x Dipirona " + precopromocional*2)
