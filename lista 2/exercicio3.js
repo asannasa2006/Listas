bac47cacd1f317671e4507ccc8e6651e7b72f778
asannasa2006 <biaanna104@gmail.com>
2024-06-18 10:53:02 -0300

    Lista 2
    Exercício 3

--- lista 2/exercicio3.js
+++ lista 2/exercicio3.js
@@ -1,4 +1,8 @@
 console.log("SuperMercado")
 
-let produto= prompt (parseInt("Digite o preço do produto: "));
+let produto= prompt (parseFloat("Digite o preço do produto: "));
 let totalparcial = produto *2;
+let totalpromocional = produto/2;
+
+console.log(produto);
+console.log("Total a pagar:" + (totalparcial + totalpromocional));
