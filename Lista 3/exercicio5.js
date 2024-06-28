4196908c0bd8eeba221f3351a918fd95671d4edf
asannasa2006 <biaanna104@gmail.com>
2024-06-28 11:04:52 -0300

    Lista 3
    exercício 5

---
+++ Lista 3/exercicio5.js
@@ -0,0 +1,7 @@
+console.log("Sucessor")
+
+let numero = parseInt(prompt ("Digite um número inteiro:"));
+
+let sucessor = (numero) + 1;
+
+console.log( "O sucessor do número digitado é:" + sucessor);
