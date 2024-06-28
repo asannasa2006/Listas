32a093f42ef2de4179da9cfb8dde7a2cd0c8cf29
asannasa2006 <biaanna104@gmail.com>
2024-06-28 09:16:59 -0300

    Lista 3
    exercício 1

---
+++ Lista 3/exercicio1.js
@@ -0,0 +1,10 @@
+console.log("Retângulo: área e perímetro");
+
+let base = prompt(parseFloat("Qual a base do Retângulo?"));
+let altura = prompt(parseFloat("Qual a altura do seu retângulo"));
+
+let area = base * altura;
+let perimetro = (2*base )+(2*altura);
+
+console.log("Área =" + area);
+console.log("Peŕimetro =" + perimetro);
