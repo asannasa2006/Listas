93dae442d1c2f86b359a86e2b6c8c891bf215a3d
biaanna <biaanna104@gmail.com>
2024-09-08 09:11:48 -0300

    Exercício 4

---
+++ lista 4/exercicio4.js
@@ -0,0 +1,16 @@
+console.log("Idade em anos");
+
+const dias_em_anos= 365.25; // para contar anos bissextos
+const dias_em_meses= 30.417; //quantidade aprox. de dias por mês.
+
+let dias = parseInt(prompt("Quantos dias você tem: "));
+
+//calcular anos
+let anos= Math.floor(dias/dias_em_anos); 
+let resto_de_dias = Math.floor(dias % dias_em_anos); //o % é o resto da divisão entre as variáveis
+
+//calcular meses
+let meses = Math.floor(resto_de_dias/dias_em_meses); // O math.floor arredenda para o menor número inteiro (ex. 34.5= 34)
+resto_de_dias = Math.floor(resto_de_dias % dias_em_meses);
+
+console.log("São aproximadamente " + anos + " ano/s, " + meses + " mês/meses e " + resto_de_dias + "dias/s")
